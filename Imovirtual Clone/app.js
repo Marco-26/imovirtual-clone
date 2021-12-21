5aa6da383de71be5abbf99fc03d4238fc0599a4f
Marco <markcostah@gmail.com>
2021-12-21 20:22:08 +0000

    dropdown nav bar

--- Imovirtual Clone/app.js
+++ Imovirtual Clone/app.js
@@ -5,3 +5,25 @@ logo.addEventListener('click',function(){
     menu.classList.toggle('showmenu');
     logo.classList.toggle('active');
 });
+
+
+const submenu_parent = document.querySelector('.submenu-parent');
+const submenu = document.querySelector('.submenu');
+
+submenu_parent.addEventListener("click", function(){
+    submenu.classList.toggle('open-submenu');
+});
+
+const submenu_parent_2 = document.querySelector('.submenu-parent-2');
+const submenu_2 = document.querySelector('.submenu-2');
+
+submenu_parent_2.addEventListener("click", function(){
+    submenu_2.classList.toggle('open-submenu');
+});
+
+const submenu_parent_3 = document.querySelector('.submenu-parent-3');
+const submenu_3 = document.querySelector('.submenu-3');
+
+submenu_parent_3.addEventListener("click", function(){
+    submenu_3.classList.toggle('open-submenu');
+});
