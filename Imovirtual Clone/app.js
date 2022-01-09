5196d1c756dabccc6eba53733994e0553a45ed8c
Marco <markcostah@gmail.com>
2022-01-09 19:16:09 +0000

    small adjustments

--- Imovirtual Clone/app.js
+++ Imovirtual Clone/app.js
@@ -23,17 +23,21 @@ function ToggleSubmenuDesktop(submenu){
 }
 
 // Função que apenas permite ter um submenu aberto de cada vez
-function CloseSubmenus(submenu1, submenu2){
+function CloseSubmenus(submenu1, submenu2, arrow1, arrow2){
     submenu1.classList.remove('active__listitemdrop') ? (w>=1215) : submenu1.classList.remove('active-listitemdrop');
     submenu2.classList.remove('active__listitemdrop') ? (w>=1215) : submenu2.classList.remove('active-listitemdrop');
+    // arrow1.classList.remove('arrow-active');
+    // arrow2.classList.remove('arrow-active');
 }
 
 // Submenu 1
 const submenu_parent_1 = document.querySelector('.nav__item-1');
 const submenu_1 = document.querySelector('.nav__drop-1');
+const arrow_1 = document.querySelector('.arrow1');
 
 submenu_parent_1.addEventListener("click",function(){
-    CloseSubmenus(submenu_2, submenu_3);
+    arrow_1.classList.toggle('arrow-active');
+    CloseSubmenus(submenu_2, submenu_3, arrow_2, arrow_3);
     if(w>=1215){
         ToggleSubmenuDesktop(submenu_1);
     }
@@ -45,8 +49,10 @@ submenu_parent_1.addEventListener("click",function(){
 // Submenu 2
 const submenu_parent_2 = document.querySelector('.nav__item-2');
 const submenu_2 = document.querySelector('.nav__drop-2');
+const arrow_2 = document.querySelector('.arrow2');
 
 submenu_parent_2.addEventListener("click", function(){
+    arrow_2.classList.toggle('arrow-active');
     CloseSubmenus(submenu_1, submenu_3);
     if(w>=1215){
         ToggleSubmenuDesktop(submenu_2);
@@ -59,8 +65,10 @@ submenu_parent_2.addEventListener("click", function(){
 // Submenu 3
 const submenu_parent_3 = document.querySelector('.nav__item-3');
 const submenu_3 = document.querySelector('.nav__drop-3');
+const arrow_3 = document.querySelector('.arrow3');
 
 submenu_parent_3.addEventListener("click", function(){
+    arrow_3.classList.toggle('arrow-active');
     CloseSubmenus(submenu_1,submenu_2);
     if(w>=1215){
         ToggleSubmenuDesktop(submenu_3);
